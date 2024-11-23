df2b0d7c03f95b0078e93cac33f63c448c115951
Iago Azevedo Costa Pereira <iago0307@gmail.com>
2024-11-23 07:49:24 -0300

    Usando o C++ 11

--- jogodaforca/jogodaforca.cpp
+++ jogodaforca/jogodaforca.cpp
@@ -2,8 +2,31 @@
 #include<string>
 using namespace std;
 
+const string PALAVRA_SECRETA = "MELANCIA";
+
+bool letra_existe(char chute) {
+	for(char letra : PALAVRA_SECRETA) {
+		if (chute == letra) {
+			return true;
+		}
+	}
+	return false;
+}
+
 int main() {
-	string palavra_aleatoria = "MELANCIA";
-	cout << palavra_aleatoria << endl;
+	cout << PALAVRA_SECRETA  << endl;
+	bool nao_errou = true;
+	bool nao_enforcou = true;
+	while(nao_errou && nao_enforcou) {
+		char chute;
+		cin >> chute;
+		cout << "Seu chute foi: " << chute << endl;
+		if (letra_existe(chute)) {
+			cout << "Voce acertou! Seu chute esta na palavra" << endl;
+		}
+		else {
+			cout << "Voce errou! Seu chute nao esta na palavra" << endl;
+		}
+	}
 	return 0;
 }
